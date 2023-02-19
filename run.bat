168b36221fd8bccb899b0a4f903bbdbd171ef992
Fernando Cruz <fernandogomescruz@gmail.com>
2023-02-18 22:57:35 -0300

    first commit

---
+++ run.bat
@@ -0,0 +1,17 @@
+@echo off
+
+
+
+echo "removendo antigo lovegame"
+
+adb shell rm -r sdcard/lovegame
+
+echo "lovegame removido"
+
+echo "enviando %1 como novo lovegame"
+
+adb shell mkdir sdcard/lovegame
+
+adb push %1/. /sdcard/lovegame/
+
+echo "%1 enviado."
